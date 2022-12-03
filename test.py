3e6a1ff7a0c45ee987a37006c78d07152d8ebc3a
Vulliary <poltavtsev.vova@gmail.com>
2022-12-03 12:18:45 +0200

    Version 3.1

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
-print("Hello world")
+# class Student:
+#     print('Hi')
+# st1 = Student()
+# st2 = Student()
+# st3 = Student()
+
+class Student:
+    education = "Step"
+
+    def __init__(self):
+        self.name = 'Oleg'
+        self.age = 15
+        self.height = 156
+        print("Все получилось♥")
+        # print(id(self))
+
+
+st1 = Student()
+print(st1.education)
+print(st1.name)
+print(st1.age)
+print(st1.height)
+st2 = Student()
